6ecb53527b68a5eb5f57f609900876962545a59d
´Alejandro <alejogi20@gmail.com>
2019-06-22 01:24:59 -0400

    Añadido el algoritmo de generacion de laberintos, falta hacer que tenga la recursividad para cada cuadrante del laberinto despues de la primera division

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,6 +1,8 @@
 
 package project;
 
+import java.util.Random;
+
 /**
  *
  * @author Goncalves
@@ -37,7 +39,7 @@ public class Main {
     
         
         
-        generateMaze(array);
+        generateMaze(array, 9, 2, 9, 8);
         
         for (int i = 0; i < 10; i++) {
             
@@ -48,8 +50,71 @@ public class Main {
         }
     }
     
-    static public void generateMaze(int[][] array){
+    public static void generateMaze(int[][] array, int rowBound, int rowEntrance, int colBound, int rowExit){
         
+        Random rand = new Random();
+        int colWall;
+        int rowWall;
+        
+        do{
+            colWall = rand.nextInt(colBound - 1);
+        }while(colWall < 2);
+        
+        do{
+            rowWall  = rand.nextInt(rowBound -1) ;
+        }while(rowWall < 2);
+            
+        for(int i = 1; i < rowBound; i++){
+                array[i][colWall] = 1;
+        }
+        
+        for (int j = 0; j < colBound; j++) {
+            array[rowWall][j] = 1;
+        }
+        
+        int flag = rand.nextInt(2);
+        
+        if(flag == 0){
+            int breakWall1, breakWall2, breakWall3;
+            
+        do{
+            breakWall1  = rand.nextInt(colBound) ;
+        }while(breakWall1 < 1 || breakWall1 >= colWall);    
+            
+        do{
+            breakWall2  = rand.nextInt(colBound) ;
+        }while(breakWall2 < 1 || breakWall2 <= colWall );    
+
+        do{
+            breakWall3  = rand.nextInt(rowBound) ;
+        }while(breakWall3 < 1 || breakWall3 == rowWall);    
+  
+        array[rowWall][breakWall1] = 0;
+        array[rowWall][breakWall2] = 0;
+        array[breakWall3][colWall] = 0;
+        
+        }else{
+           
+             int breakWall1, breakWall2, breakWall3;
+            
+        do{
+            breakWall1  = rand.nextInt(rowBound) ;
+        }while(breakWall1 < 1 || breakWall1 >= rowWall);    
+            
+        do{
+            breakWall2  = rand.nextInt(rowBound) ;
+        }while(breakWall2 < 1 || breakWall2 <= rowWall );    
+
+        do{
+            breakWall3  = rand.nextInt(colBound) ;
+        }while(breakWall3 < 1 || breakWall3 == colWall);    
+  
+        array[breakWall1][colWall] = 0;
+        array[breakWall2][colWall] = 0;
+        array[rowWall][breakWall3] = 0;
+        
+            
+        }
         
     }
 }
