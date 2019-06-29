9eb49c112e836709eb07c85eac6d2049e5a690cb
´Alejandro <alejogi20@gmail.com>
2019-06-29 13:10:54 -0400

    añadido el metodo de division, pero aun sin recursividad, no unciona completamente

--- src/project/Main.java
+++ src/project/Main.java
@@ -11,110 +11,112 @@ public class Main {
 
     public static void main(String[] args) {
         
-        int[][]  array = new int[10][10];
-        
-        for (int x = 0; x < 10; x++) {
-            for (int y = 0; y < 10; y++) {
-                array[x][y] = 0;
-            }
-        }
+        String[][]  array = new String[30][30];
         
         
-        for (int i = 0; i < 10; i++) {
+        generateMaze(array, 0, array.length, 0, array[0].length , 5, 0, 0, 8, 0);
+        
+        
+      
+        
+        for (int i = 0; i < array.length; i++) {
             
-            for (int j = 0; j < 10; j++) {
-                
-                if(i == 0 || i == 9){
-                    array[i][j] = 1;
-                }else if(i == 1 ){
-                    array[i][9] = 1;
-                    break;
-                }else if( i == 8){
-                    array[i][0] = 1;
-                    break;
-                } else if(j == 0 || j == 9) array[i][j] = 1;
-                
-            }
-        }
-    
-        
-        
-        generateMaze(array, 9, 2, 9, 8);
-        
-        for (int i = 0; i < 10; i++) {
-            
-            for (int j = 0; j < 10; j++) {
-                System.out.print("  " + array[i][j]);
-               if(j == 9) System.out.print(" \n"); 
+            for (int j = 0; j < array[0].length; j++) {
+                System.out.print(" " +array[i][j]);
+               if(j == array.length - 1) System.out.print(" \n"); 
             }
         }
     }
     
-    public static void generateMaze(int[][] array, int rowBound, int rowEntrance, int colBound, int rowExit){
+    public static void generateMaze(String[][] maze, int topRow, int bottomRow, int leftCol, int rightCol, int entranceRow, int entranceCol, int exitRow, int exitCol, int wallRotation){ 
+        /* 
+            La condición de parada, si la diferencia entre las paredes (que son el tope o limites de cada cuadrante)
+           es igual a 2, entonces es porque ya no se puede seguir dividiendo ese cuadrante, entonces retorna.
+        */
+        
+        if(bottomRow - topRow == 2  ||  rightCol - leftCol == 2) return; 
         
         Random rand = new Random();
-        int colWall;
-        int rowWall;
+        int cWall;
+        int rWall;
         
-        do{
-            colWall = rand.nextInt(colBound - 1);
-        }while(colWall < 2);
+        /* 
+            esto rellena los bordes de la matriz, si la primera celda es null entonces todas las demas son null
+           esto quiere decir que no tiene nada, entonces este codigo no solo rellena los bordes sino que pone 
+           los caracteres que corresponden a " caminos "
+        */
         
-        do{
-            rowWall  = rand.nextInt(rowBound -1) ;
-        }while(rowWall < 2);
-            
-        for(int i = 1; i < rowBound; i++){
-                array[i][colWall] = 1;
+        if( maze[0][0] == null ){
+            for (int r = 0; r < maze.length; r++) {
+                for (int c = 0; c < maze[0].length; c++) {
+                    
+                    if( (r != 0 && r != maze.length - 1) && (c != 0 && c != maze[0].length - 1)) maze[r][c] = "≡";
+                    else if( r == entranceRow && c == entranceCol ) maze[r][c]   = "□";
+                    else if(r == exitRow && c == exitCol) maze[r][c] = "∩";
+                    else maze[r][c] = "■";
+                }
+            }
         }
         
-        for (int j = 0; j < colBound; j++) {
-            array[rowWall][j] = 1;
-        }
+        /* 
+           Por ahora este if es para saber si se va a poner un pared como columna o como fila, 
+           cuando wallRotation es igual a 0 entonces se hará un pared fila o horizontal, si es 1 entonces se
+           hará una pared columna o vertical
+        */
         
-        int flag = rand.nextInt(2);
-        
-        if(flag == 0){
-            int breakWall1, breakWall2, breakWall3;
-            
-        do{
-            breakWall1  = rand.nextInt(colBound) ;
-        }while(breakWall1 < 1 || breakWall1 >= colWall);    
-            
-        do{
-            breakWall2  = rand.nextInt(colBound) ;
-        }while(breakWall2 < 1 || breakWall2 <= colWall );    
-
-        do{
-            breakWall3  = rand.nextInt(rowBound) ;
-        }while(breakWall3 < 1 || breakWall3 == rowWall);    
-  
-        array[rowWall][breakWall1] = 0;
-        array[rowWall][breakWall2] = 0;
-        array[breakWall3][colWall] = 0;
-        
-        }else{
+        if(wallRotation == 0){
            
-             int breakWall1, breakWall2, breakWall3;
+            /*
+              BreakCellAtCol es " romper una celda en tal columna " esta es la celda que vamos a romper 
+              una vez que tengamos hecha la pared como es horizontal, entonces la variable contendrá una
+              posición de una columna random donde se va a romper o hacer un hueco (no confundir con la 
+              entrada y la salida del laberinto)
+            */
+            int breakCellAtCol; 
             
-        do{
-            breakWall1  = rand.nextInt(rowBound) ;
-        }while(breakWall1 < 1 || breakWall1 >= rowWall);    
+            /* 
+                estos do-while asignan las posiciones random, hay muchas condiciones que tiene que cumplir
+                un numero random para ser asignado, para que una pared no tape la salida ni la entrada ni 
+                los nuevos huecos
+           */
             
-        do{
-            breakWall2  = rand.nextInt(rowBound) ;
-        }while(breakWall2 < 1 || breakWall2 <= rowWall );    
+            //la posicion random de la fila donde se hará la pared
+            do{
+                rWall = rand.nextInt(bottomRow);
+            }while(rWall == entranceRow || rWall == exitRow || ((rWall == entranceRow - 1 && entranceRow == bottomRow) || ( rWall == entranceRow + 1 && entranceRow == topRow ) ) || (rWall == entranceRow - 1 && exitRow == bottomRow) || ( rWall == exitRow + 1 && exitRow == topRow) );
+            
+            // la posición random en la columna donde se hará el hueco en la pared vertical
+            do{
+                breakCellAtCol = rand.nextInt(rightCol);
+            }while(breakCellAtCol <= leftCol);
+            
+            //este for hace la pared, y no rellena donde se supone que va el hueco
+            for (int c = leftCol + 1; c < rightCol; c++) {
+                if(c != breakCellAtCol) maze[rWall][c] = "■";
+            }
+            
+        }    
+          
+        // este es para hacer paredes pero verticales, como se indicó antes
+        if(wallRotation == 1){
+            int breakCellAtRow; // hueco en una fila random
+            
+            // pared vertical en posicion random
+            do{
+                cWall = rand.nextInt(rightCol) + 1;
+            }while( cWall == exitCol || ((cWall == entranceCol - 1 && entranceCol == rightCol) || ( cWall == entranceCol + 1 && entranceCol == leftCol ) ) || (cWall == exitCol - 1 && exitCol == rightCol) || ( cWall == exitCol + 1 && exitCol == leftCol) );
+            
+            //romper una celda en una fila random de la pared
+            do{
+                breakCellAtRow = rand.nextInt(bottomRow) + topRow;
+            }while(breakCellAtRow == topRow);
 
-        do{
-            breakWall3  = rand.nextInt(colBound) ;
-        }while(breakWall3 < 1 || breakWall3 == colWall);    
-  
-        array[breakWall1][colWall] = 0;
-        array[breakWall2][colWall] = 0;
-        array[rowWall][breakWall3] = 0;
-        
-            
+            //hacer la pared 
+            for (int r = topRow + 1; r < bottomRow ; r++) {
+                if(breakCellAtRow != r) maze[r][cWall] = "■";
+            }
+
         }
-        
     }
+    
 }
